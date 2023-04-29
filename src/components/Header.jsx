5aca31065a3fe6d059f76f6c4b648e2e404cc353
Jakub Kawka <jakubkawka2005@gmail.com>
2023-04-29 15:16:00 +0200

    hmm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,72 @@
+import { useEffect } from 'react';
 import '../styles/header.css';
 
 const Header = () => {
-    return ( 
+
+    useEffect(() => {
+        loop()
+    }, [])
+
+    const loop = () => {
+
+        const element = document.querySelector('#logo')
+        setTimeout(() => {
+            element.textContent = '_'
+            setTimeout(() => {
+                element.textContent = ''
+                setTimeout(() => {
+                    element.textContent = '_'
+                    setTimeout(() => {
+                        element.textContent = 'C_'
+                        setTimeout(() => {
+                            element.textContent = 'Co_'
+                            setTimeout(() => {
+                                element.textContent = 'Cod_'
+                                setTimeout(() => {
+                                    element.textContent = 'Code_'
+                                    setTimeout(() => {
+                                        element.textContent = 'Codes'
+                                        setTimeout(() => {
+                                            element.textContent = 'Codes'
+                                            setTimeout(() => {
+                                                element.textContent = 'Codes'
+                                                setTimeout(() => {
+                                                    element.textContent = 'Code_'
+                                                    setTimeout(() => {
+                                                        element.textContent = 'Cod_'
+                                                        setTimeout(() => {
+                                                            element.textContent = 'Co_'
+                                                            setTimeout(() => {
+                                                                element.textContent = 'C_'
+                                                                setTimeout(() => {
+                                                                    element.textContent = '_'
+                                                                    setTimeout(() => {
+                                                                        element.textContent = ''
+                                                                        loop()
+                                                                    }, 400)
+                                                                }, 400)
+                                                            }, 400)
+                                                        }, 400)
+                                                    }, 400)
+                                                }, 400)
+                                            }, 400)
+                                        }, 10000)
+                                    }, 400)
+                                }, 400)
+                            }, 400)
+                        }, 400)
+                    }, 400)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+
+    }
+
+    return (
         <div className="header">
-            <main>   
+            <main>
                 <div className="logo">
-                    GoodieCodes
+                    Goodie<span id='logo'></span>
                 </div>
                 <div className="quote">
                     Witaj, interesuje Cię nasza firma? Pracuj dla nas!
@@ -15,5 +76,5 @@ const Header = () => {
         </div>
     );
 }
- 
+
 export default Header;
